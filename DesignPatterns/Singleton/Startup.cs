2410a700a5aabbaeb14ded930f6b413d9d4b538a
Bruno de Andrade <bruno.andrade23@gmail.com>
2020-05-31 19:57:47 +0200

    Add singleton example

---
+++ DesignPatterns/Singleton/Startup.cs
@@ -0,0 +1,21 @@
+﻿using Autofac;
+using System;
+
+namespace DesignPatterns.Singleton
+{
+    class Startup
+    {
+        static void Main(string[] args)
+        {
+            // Basic singleton class
+            Console.WriteLine($"Is Singleton? {SingletonTester.IsSingleton(() => SimpleSingletonDatabase.Instance)}");
+
+            // Class not singleton by itself but by dependency injection
+            var builder = new ContainerBuilder();
+            builder.RegisterType<OrdinaryDatabase>().As<IDatabase>().SingleInstance();
+            var container = builder.Build();
+
+            Console.WriteLine($"Is Singleton? {SingletonTester.IsSingleton(() => container.Resolve<IDatabase>())}");
+        }
+    }
+}
