a9ea978fa6bf93bb962b3d9139d9a60b41a0d1c4
Bruno de Andrade <bruno.andrade23@gmail.com>
2020-06-04 23:24:29 +0200

    Add Composite pattern example

---
+++ DesignPatterns/Composite/IValueContainer.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+
+namespace DesignPatterns.Composite
+{
+    interface IValueContainer : IEnumerable<int>
+    {
+    }
+}
---
+++ DesignPatterns/Composite/SingleValue.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace DesignPatterns.Composite
+{
+    class SingleValue : IValueContainer
+    {
+        public int Value { get; set; }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            yield return Value;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
