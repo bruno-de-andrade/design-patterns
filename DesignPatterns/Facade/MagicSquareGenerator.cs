1c82f3e87c9da2c4e30c830e049385b66c29f312
Bruno de Andrade <bruno.andrade23@gmail.com>
2020-06-06 18:00:20 +0200

    Add Facade example

---
+++ DesignPatterns/Facade/MagicSquareGenerator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.Facade
+{
+    public static class MagicSquareGenerator
+    {
+        public static List<List<int>> Generate(int size)
+        {
+            List<List<int>> matrix;
+            List<List<int>> list;
+            int tries = 0;
+
+            do
+            {
+                list = new List<List<int>>();
+
+                for (int index = 0; index < size; index++)
+                {
+                    list.Add(Generator.Generate(size));
+                }
+
+                matrix = Splitter.Split(list);
+                tries++;
+            }
+            while (!Verifier.Verify(matrix));
+
+            return list;
+        }
+    }
+}
---
+++ DesignPatterns/Facade/Splitter.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+
+namespace DesignPatterns.Facade
+{
+    public static class Splitter
+    {
+        /// <summary>
+        /// Splits a 2D square matrix into several lists containing all rows, all columns and all diagonals.
+        /// </summary>
+        public static List<List<int>> Split(List<List<int>> array)
+        {
+            var result = new List<List<int>>();
+            var rowCount = array.Count;
+            var colCount = array[0].Count;
+
+            // get the rows
+            for (int rowIndex = 0; rowIndex < rowCount; ++rowIndex)
+            {
+                var row = new List<int>();
+
+                for (int colIndex = 0; colIndex < colCount; ++colIndex)
+                    row.Add(array[rowIndex][colIndex]);
+
+                result.Add(row);
+            }
+
+            // get the columns
+            for (int colIndex = 0; colIndex < colCount; ++colIndex)
+            {
+                var column = new List<int>();
+                
+                for (int rowIndex = 0; rowIndex < rowCount; ++rowIndex)
+                    column.Add(array[rowIndex][colIndex]);
+                
+                result.Add(column);
+            }
+
+            // now the diagonals
+            var diagonal1 = new List<int>();
+            var diagonal2 = new List<int>();
+            for (int colIndex = 0; colIndex < colCount; ++colIndex)
+            {
+                for (int rowIndex = 0; rowIndex < rowCount; ++rowIndex)
+                {
+                    if (colIndex == rowIndex)
+                        diagonal1.Add(array[rowIndex][colIndex]);
+
+                    var r2 = rowCount - rowIndex - 1;
+
+                    if (colIndex == r2)
+                        diagonal2.Add(array[rowIndex][colIndex]);
+                }
+            }
+
+            result.Add(diagonal1);
+            result.Add(diagonal2);
+
+            return result;
+        }
+    }
+}
