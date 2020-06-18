731df24d8f8f4167f288c81f5d47793db4bfeaa1
Bruno de Andrade <bruno.andrade23@gmail.com>
2020-06-18 19:06:56 +0200

    Add Observer pattern example

---
+++ DesignPatterns/Observer/Game.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DesignPatterns.Observer
+{
+    public class Game
+    {
+        public event EventHandler RatEntersGame, RatDies;
+
+        public event EventHandler<Rat> RatNotifyOtherRatAboutHisPresence;
+
+        public void AddRat(object sender)
+        {
+            RatEntersGame?.Invoke(sender, null);
+        }
+
+        public void RemoveRat(object sender)
+        {
+            RatDies?.Invoke(sender, null);
+        }
+
+        public void NotifyRatAboutOthersPresence(object sender, Rat rat)
+        {
+            RatNotifyOtherRatAboutHisPresence?.Invoke(sender, rat);
+        }
+    }
+}
